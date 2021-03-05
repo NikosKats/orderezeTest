9e04a90d83ace89b2313dfb9df53f9d58ce5ae96
Nikolaos Katsilidis <nikolaoskatsilidis@Galaxy.station>
2021-03-05 12:34:50 +0200

    create new

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios'
-import {Container, Form, Row, Col} from 'react-bootstrap'
+import {Container, Form, Row, Col, Button} from 'react-bootstrap'
 import ControlledOpenSelect from './ControlledOpenSelect'
 
 class Create extends Component {
@@ -34,10 +34,42 @@ class Create extends Component {
                                 <ControlledOpenSelect/>
                             </Col>
                             <Col>
-                                <Form.Label>Published</Form.Label>
-                                <Form.Control placeholder="Published" disabled />
+                            
+                                <Form.Label> Is Page Active </Form.Label>
+                                <Row sm={10} >
+
+                                    <Form.Check
+                                    type="radio"
+                                    label="Active"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios1"
+                                    style={{marginLeft:15,marginRight: 15}}
+                                    />
+
+                                    <Form.Check
+                                    type="radio"
+                                    label="Not Active"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios2"
+                                    style={{marginLeft:15,marginRight: 15}}
+                                    />
+                                </Row>
+                            
+                                
                             </Col>
                         </Row>
+
+                        <Row>
+                            <Col>
+                                <Form.Label>Published</Form.Label>
+                                <Form.Control placeholder="Published" disabled/>
+                            </Col>
+                        </Row>
+
+                        <Button variant="primary" size="lg" block style={{marginTop:30}}>
+                            Submit Page
+                        </Button>
+
                     </Form>
                 </Container>
             </div>
