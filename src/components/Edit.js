a870b6c273304c0a56f8d31c90531c7aabd9c7ee
Nikolaos Katsilidis <nikolaoskatsilidis@Galaxy.station>
2021-03-05 12:46:27 +0200

    edit page

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -4,10 +4,30 @@ import axios from 'axios'
 import {Container, Form, Row, Col, Button} from 'react-bootstrap'
 import ControlledOpenSelect from './ControlledOpenSelect'
 
+
+const api = axios.create({
+    baseURL: 'https://pagesmanagement.azurewebsites.net/'
+  })
+
 class Edit extends Component {
+
+    state = {
+        pages: []
+        
+    }
 
     constructor(){
         super();
+        this.getPages();
+    }
+
+    getPages = async () => {
+        let data = await api.get('/api/ResponsivePages/'+'3208').then(({ data }) => data);
+        
+        this.setState({ 
+            pages: data,
+            count: data.length
+        })
     }
 
     render(){
@@ -20,11 +40,11 @@ class Edit extends Component {
                         <Row style={{marginTop:15,marginBottom: 15}}>
                             <Col>
                                 <Form.Label>Title</Form.Label>
-                                <Form.Control placeholder="Title" />
+                                <Form.Control placeholder={this.state.pages.title} />
                             </Col>
                             <Col>
                                 <Form.Label>Description</Form.Label>
-                                <Form.Control placeholder="Description" />
+                                <Form.Control placeholder={this.state.pages.description} />
                             </Col>
                         </Row>
 
@@ -35,23 +55,25 @@ class Edit extends Component {
                             </Col>
                             <Col>
                             
-                                <Form.Label> Is Page Active </Form.Label>
+                                <Form.Label> Is Page Active  </Form.Label>
                                 <Row sm={10} >
 
                                     <Form.Check
-                                    type="radio"
-                                    label="Active"
-                                    name="formHorizontalRadios"
-                                    id="formHorizontalRadios1"
-                                    style={{marginLeft:15,marginRight: 15}}
+                                        type="radio"
+                                        label="Active"
+                                        name="formHorizontalRadios"
+                                        id="formHorizontalRadios1"
+                                        style={{marginLeft:15,marginRight: 15}}
+                                        checked = {this.state.pages.isActive}
                                     />
 
                                     <Form.Check
-                                    type="radio"
-                                    label="Not Active"
-                                    name="formHorizontalRadios"
-                                    id="formHorizontalRadios2"
-                                    style={{marginLeft:15,marginRight: 15}}
+                                        type="radio"
+                                        label="Not Active"
+                                        name="formHorizontalRadios"
+                                        id="formHorizontalRadios2"
+                                        style={{marginLeft:15,marginRight: 15}}
+                                        checked = {this.state.pages.isActive}
                                     />
                                 </Row>
                             
@@ -62,7 +84,7 @@ class Edit extends Component {
                         <Row>
                             <Col>
                                 <Form.Label>Published</Form.Label>
-                                <Form.Control placeholder="Published" disabled/>
+                                <Form.Control placeholder={this.state.pages.publishedOn} disabled/>
                             </Col>
                         </Row>
 
