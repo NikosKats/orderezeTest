9353d9bef376aa9bd43533d36d0c74f924a8f63f
Nikolaos Katsilidis <nikolaoskatsilidis@Galaxy.station>
2021-03-05 12:36:07 +0200

    edit page

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios'
+import {Container, Form, Row, Col, Button} from 'react-bootstrap'
+import ControlledOpenSelect from './ControlledOpenSelect'
 
 class Edit extends Component {
 
@@ -11,7 +13,65 @@ class Edit extends Component {
     render(){
         return(
             <div>
-                Hello from Edit
+                <h1 className="text-center" style={{marginTop:15,marginBottom: 15}}>Edit Page</h1>
+                <Container>
+                    <Form>
+
+                        <Row style={{marginTop:15,marginBottom: 15}}>
+                            <Col>
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control placeholder="Title" />
+                            </Col>
+                            <Col>
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control placeholder="Description" />
+                            </Col>
+                        </Row>
+
+                        <Row style={{marginTop:15,marginBottom: 15}}>
+                            <Col>
+                                <Form.Label>Page Type</Form.Label>
+                                <ControlledOpenSelect/>
+                            </Col>
+                            <Col>
+                            
+                                <Form.Label> Is Page Active </Form.Label>
+                                <Row sm={10} >
+
+                                    <Form.Check
+                                    type="radio"
+                                    label="Active"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios1"
+                                    style={{marginLeft:15,marginRight: 15}}
+                                    />
+
+                                    <Form.Check
+                                    type="radio"
+                                    label="Not Active"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios2"
+                                    style={{marginLeft:15,marginRight: 15}}
+                                    />
+                                </Row>
+                            
+                                
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col>
+                                <Form.Label>Published</Form.Label>
+                                <Form.Control placeholder="Published" disabled/>
+                            </Col>
+                        </Row>
+
+                        <Button variant="primary" size="lg" block style={{marginTop:30}}>
+                            Update Page
+                        </Button>
+
+                    </Form>
+                </Container>
             </div>
     )}
 }
